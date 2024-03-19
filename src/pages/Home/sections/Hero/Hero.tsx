8f9c28fb6b1a7c3428e728849a569e34ce5fbe97
Janaina Bacin <114409955+janaBR30@users.noreply.github.com>
2024-03-19 18:10:09 -0300

    changes

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Container, Grid, Typography, styled } from "@mui/material"
+import { Box, Container, Grid, Typography, styled } from "@mui/material"
 import Avatar from "../../../../assets/images/pexels-thisisengineering-3861958.jpg"
 import DownloadIcon from '@mui/icons-material/Download';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
